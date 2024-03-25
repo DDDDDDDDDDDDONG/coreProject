38374f9486e1c07eb96f4d8fc545220ce9d3b35e
SMHRD <SMHRD@211.105.164.253>
2024-03-25 10:10:46 +0900

    memberController에 join메소드 수정

--- Spring4cp/src/main/java/com/smhrd/controller/MemberController.java
+++ Spring4cp/src/main/java/com/smhrd/controller/MemberController.java
@@ -1,10 +1,16 @@
 package com.smhrd.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.smhrd.entity.Member;
 import com.smhrd.mapper.BoardMapper;
 import com.smhrd.mapper.MemberMapper;
@@ -21,7 +27,49 @@ public class MemberController {
 
 	
 	@RequestMapping
-	public String join() {
+	public String join(HttpServletRequest request) {
+		
+		MultipartRequest multi = null;
+		
+		int fileMaxSize = 10 * 1024 * 10000;
+		String savePath = request.getRealPath("resources/board");
+		System.out.println(savePath);
+		
+		try {
+			multi = new MultipartRequest(request, savePath, fileMaxSize, "UTF-8", new DefaultFileRenamePolicy());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String id = multi.getParameter("id");
+		String pw = multi.getParameter("pw");
+		String nick= multi.getParameter("nick");
+		String name= multi.getParameter("name");
+		String email= multi.getParameter("email");
+		String birth= multi.getParameter("birth");
+		String phone= multi.getParameter("phone");
+		String grade= multi.getParameter("grade");
+		String scname= multi.getParameter("scname");
+		String scid= multi.getParameter("scid");
+		String status= multi.getParameter("status");
+		String imgpath= multi.getParameter("imgpath");
+		
+		Member vo = new Member();
+		vo.setId(id);
+		vo.setPw(pw);
+		vo.setNick(nick);
+		vo.setName(name);
+		vo.setEmail(email);
+		vo.setBirth(birth);
+		vo.setPhone(phone);
+		vo.setGrade(grade);
+		vo.setScname(scname);
+		vo.setScid(scid);
+		vo.setStatus(status);
+		vo.setImgpath(imgpath);
+		
+		
 		mapper.join(vo);
 		
 		return "wait";
